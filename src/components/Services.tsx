f2325a0217bc5805ab1d8c4d9b5280e410fb7148
github username <annacarey@icloud.com>
2020-06-17 16:03:08 -0600

    removed yarn lock file

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import office from '../images/office.jpeg'
+import office from '../images/office.jpeg';
 import Card from './Card';
 
 const Services: React.FC = () => {
