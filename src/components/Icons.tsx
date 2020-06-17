12f1e9f2996648882c93531f7f654147f86fe5bb
github username <annacarey@icloud.com>
2020-06-16 19:13:27 -0600

    added services section

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -4,27 +4,29 @@ import styled from 'styled-components';
 const Services: React.FC = () => {
   return (
     <Wrapper>
+      <Headline>What We Love</Headline>
       <IconWrapper>
-        <Logo />
-        <Text>Systems</Text>
-        <Text>Architecture</Text>
+        <Icon>
+          <Logo />
+          <Text>Systems</Text>
+          <Text>Architecture</Text>
+        </Icon>
+        <Icon>
+          <Logo />
+          <Text>Retention</Text>
+          <Text>& CRM</Text>
+        </Icon>
+        <Icon>
+          <Logo />
+          <Text>UX</Text>
+          <Text>& CRO</Text>
+        </Icon>
+        <Icon>
+          <Logo />
+          <Text>Growth</Text>
+          <Text>Training</Text>
+        </Icon>
       </IconWrapper>
-      <IconWrapper>
-        <Logo />
-        <Text>Retention</Text>
-        <Text>& CRM</Text>
-      </IconWrapper>
-      <IconWrapper>
-        <Logo />
-        <Text>UX</Text>
-        <Text>& CRO</Text>
-      </IconWrapper>
-      <IconWrapper>
-        <Logo />
-        <Text>Growth</Text>
-        <Text>Training</Text>
-      </IconWrapper>
-      <IconWrapper />
     </Wrapper>
   );
 };
@@ -35,12 +37,17 @@ const Wrapper = styled.div`
   width: 100%;
   height: 30vh;
   display: flex;
-  align-items: center;
-  justify-content: space-evenly;
+  flex-direction: column;
   background-color: #1c7975;
 `;
 
 const IconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+const Icon = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -58,4 +65,12 @@ const Logo = styled.div`
   background-color: grey;
   width: 50px;
   height: 50px;
+`;
+
+const Headline = styled.h1`
+  font-size: 20px;
+  color: white;
+  margin-top: 20px;
+  margin-bottom: 30px;
+  text-align: center;
 `;
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -4,11 +4,27 @@ import styled from 'styled-components';
 const Services: React.FC = () => {
   return (
     <Wrapper>
-      <Text>
-        {' '}
-        We help marketing and product teams create sustainable growth engines
-        that mulitply revenue.
-      </Text>
+      <Headline>Our Services</Headline>
+      <CardWrapper>
+        <Card>
+          <Text>Engagement &</Text>
+          <Text>Retention Audit</Text>
+        </Card>
+        <Card>
+          <Text>Launch</Text>
+          <Text>Maximization</Text>
+          <Text>Team</Text>
+        </Card>
+        <Card>
+          <Text>Marketing</Text>
+          <Text>Automation</Text>
+          <Text>Launchpad</Text>
+        </Card>
+        <Card>
+          <Text>Growth Strategy</Text>
+          <Text>Consulting</Text>
+        </Card>
+      </CardWrapper>
     </Wrapper>
   );
 };
@@ -17,15 +33,41 @@ export default Services;
 
 const Wrapper = styled.div`
   width: 100%;
-  height: 20vh;
+  height: 50vh;
   display: flex;
-  align-items: center;
   justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const CardWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const Card = styled.div`
+  width: 130px;
+  height: 200px;
+  background-color: #20545e;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  margin: 5px;
+`;
+
+const Headline = styled.h1`
+  font-size: 20px;
+  color: #20545e;
+  margin-top: 20px;
+  margin-bottom: 30px;
+  text-align: center;
+  color: 20545e;
 `;
 
 const Text = styled.h1`
-  font-size: 20px;
-  color: #1c7975;
-  margin: 30px;
+  font-size: 14px;
+  color: white;
+  margin: 0px;
   text-align: center;
 `;
